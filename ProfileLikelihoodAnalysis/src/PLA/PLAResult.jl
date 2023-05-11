2d5b9d881f92bd5c9040e07cd7154e1de3e757a5
max-de-rooij <54850292+max-de-rooij@users.noreply.github.com>
2023-05-11 14:02:18 +0200

    Final Benchmarks Done

--- ProfileLikelihoodAnalysis/src/PLA/PLAResult.jl
+++ ProfileLikelihoodAnalysis/src/PLA/PLAResult.jl
@@ -18,9 +18,17 @@ append!(x::PLAResult, par, obj) = begin
     Base.append!(x.obj, obj)
 end
 
-Plots.plot(result::PLAResult; xlabel="Parameter Value", ylabel="Objective Function Value", titlefontsize=10, titlefontfamily="Helvetica Bold", legend = true, title="", kwargs...) = begin
-    pl = Plots.plot(result.p, result.obj, linewidth=2, labels="", xlabel=xlabel, ylabel=ylabel, palette=:seaborn_colorblind, title=title, titlefontsize=titlefontsize, titlefontfamily=titlefontfamily, kwargs...)
-    Plots.scatter!(pl, [result.initial_p], [result.initial_obj], labels=legend ? "Optimized Parameter Value" : "", markershape=:star, markersize=8, palette=:seaborn_colorblind)
-    Plots.hline!(pl, [result.initial_obj+result.threshold], labels=legend ? "Chi-square Threshold" : "", palette=:seaborn_colorblind)
+@recipe function f(result::PLAResult)
+    linecolor   --> :blue
+    seriestype  :=  :path
+    markershape --> (add_marker ? :circle : :none)
+    delete!(plotattributes, :add_marker)
+    rand(n)
+end
+
+Plots.plot(result::PLAResult; kwargs...) = begin
+    pl = Plots.plot(result.p, result.obj, linewidth=2, labels=""; kwargs...)
+    Plots.scatter!(pl, [result.initial_p], [result.initial_obj], labels="Optimized Parameter Value", markershape=:star, markersize=8, palette=:seaborn_colorblind)
+    Plots.hline!(pl, [result.initial_obj+result.threshold], labels="Chi-square Threshold", palette=:seaborn_colorblind)
     pl
 end
